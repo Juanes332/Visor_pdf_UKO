58692d63dafb1d69afa36510e2ba6af3edbdb652
Juan Hernandez <juan.hernandez@gse.com.co>
2023-10-19 09:55:40 -0500

    Miniaturas generadas

--- src/app/components/PDFViewer/PDFViewer.jsx
+++ src/app/components/PDFViewer/PDFViewer.jsx
@@ -16,6 +16,7 @@ import ImageIcon from '@material-ui/icons/Image';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import FileUploader from './FileUploader';
+
 import './pdf.css';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
@@ -26,14 +27,26 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     alignItems: 'center',
     minHeight: '100vh',
+    overflow: 'hidden',
     [theme.breakpoints.down(1355)]: {
       '& .miniatures': {
-        display: 'block',
+        display: 'none',
       },
     },
   },
+  pdfViewer: {
+    flex: 1,
+  },
   miniatures: {
     display: 'block',
+    overflow: 'auto',
+  },
+  thumbnailImage: {
+    width: '100%',
+    cursor: 'pointer',
+  },
+  selectedThumbnail: {
+    border: `2px solid ${theme.palette.primary.main}`,
   },
 }));
 
@@ -43,6 +56,7 @@ const PdfViewer = () => {
   const [pageNumber, setPageNumber] = useState(1);
   const [selectedFile, setSelectedFile] = useState(null);
   const [showFileUploader, setShowFileUploader] = useState(true);
+  const [thumbnails, setThumbnails] = useState([]);
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
@@ -73,22 +87,28 @@ const PdfViewer = () => {
     setShowFileUploader(true);
   };
 
+  const handleThumbnailClick = (page) => {
+    setPageNumber(page);
+  };
+
   return (
     <Container maxWidth="lg" className={classes.container}>
       {showFileUploader ? (
         <FileUploader onFileSelected={handleFileSelected} />
       ) : (
-        <Grid container spacing={2}>
+        <Grid container spacing={2} className={classes.pdfViewer}>
           <Grid item xs={12} sm={9}>
-            <Document file={selectedFile} onLoadSuccess={onDocumentLoadSuccess}>
-              <Page
-                pageNumber={pageNumber}
-                width={isMobile ? 300 : 800}
-                renderTextLayer={false}
-                renderAnnotationLayer={false}
-                style={{ maxWidth: 'min-content' }}
-              />
-            </Document>
+            {selectedFile && (
+              <Document file={selectedFile} onLoadSuccess={onDocumentLoadSuccess}>
+                <Page
+                  pageNumber={pageNumber}
+                  width={isMobile ? 300 : 800}
+                  renderTextLayer={!isMobile} // Renderizar la capa de texto solo en dispositivos no móviles
+                  renderAnnotationLayer={false}
+                  style={{ maxWidth: 'min-content' }}
+                />
+              </Document>
+            )}
             <div style={{ marginTop: '20px', display: 'flex', justifyContent: 'center' }}>
               <IconButton onClick={handlePreviousPage} disabled={pageNumber <= 1}>
                 <ChevronLeftIcon />
@@ -102,11 +122,25 @@ const PdfViewer = () => {
             <Paper style={{ maxHeight: '70vh', overflowY: 'auto' }}>
               <List>
                 {Array.from({ length: numPages }, (_, i) => (
-                  <ListItem button key={i} onClick={() => setPageNumber(i + 1)}>
+                  <ListItem
+                    key={i}
+                    button
+                    onClick={() => handleThumbnailClick(i + 1)}
+                    className={pageNumber === i + 1 ? classes.selectedThumbnail : null}
+                  >
                     <ListItemIcon>
-                      <ImageIcon />
+                      {selectedFile && (
+                        <Document file={selectedFile}>
+                          <Page
+                            pageNumber={i + 1}
+                            width={150}
+                            renderTextLayer={!isMobile} // Renderizar la capa de texto solo en dispositivos no móviles
+                            renderAnnotationLayer={false}
+                            style={{ maxWidth: 'min-content' }}
+                          />
+                        </Document>
+                      )}
                     </ListItemIcon>
-                    Página {i + 1}
                   </ListItem>
                 ))}
               </List>
