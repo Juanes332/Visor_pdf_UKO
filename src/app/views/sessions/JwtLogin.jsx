9677200765d31c7b17c34c97fa69a2651bdc06f6
Juan Hernandez <juan.hernandez@gse.com.co>
2023-10-27 14:07:52 -0500

    Creación del login

--- src/app/views/sessions/JwtLogin.jsx
+++ src/app/views/sessions/JwtLogin.jsx
@@ -1,45 +1,58 @@
 import { LoadingButton } from '@mui/lab';
-import { Card, Checkbox, Grid, TextField } from '@mui/material';
-import { Box, styled, useTheme } from '@mui/system';
-import { Paragraph } from 'app/components/Typography';
+import { Grid, TextField, useTheme, Typography, useMediaQuery, Checkbox } from '@mui/material';
+import { Box, styled } from '@mui/system';
 import useAuth from 'app/hooks/useAuth';
 import { Formik } from 'formik';
 import { useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import * as Yup from 'yup';
 
-const FlexBox = styled(Box)(() => ({ display: 'flex', alignItems: 'center' }));
+const FlexBox = styled(Box)({
+  display: 'flex',
+  alignItems: 'center',
+});
 
-const JustifyBox = styled(FlexBox)(() => ({ justifyContent: 'center' }));
+const FullScreenWrapper = styled(Box)({
+  display: 'flex',
+  height: '100vh',
+});
 
-const ContentBox = styled(Box)(() => ({
-  height: '100%',
+const HalfScreen = styled(Box)({
+  flex: '1',
   padding: '32px',
-  position: 'relative',
-  background: 'rgba(0, 0, 0, 0.01)',
-}));
+});
 
-const JWTRoot = styled(JustifyBox)(() => ({
-  background: '#1A2038',
-  minHeight: '100% !important',
-  '& .card': {
-    maxWidth: 800,
-    minHeight: 400,
-    margin: '1rem',
-    display: 'flex',
-    borderRadius: 12,
-    alignItems: 'center',
-  },
-}));
+const HalfScreenContent = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+  textAlign: 'center',
+});
 
-// inital login credentials
+const ImagePlaceholder = styled(Box)({
+  height: '150px',
+  width: '150px',
+  background: '#ccc',
+  marginBottom: '20px',
+});
+
+const FormScreen = styled(Box)({
+  flex: '1',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '32px',
+});
+
 const initialValues = {
   email: 'pruebastodos@yopmail.com',
   password: 'dummyPass',
   remember: true,
 };
 
-// form field validation schema
 const validationSchema = Yup.object().shape({
   password: Yup.string()
     .min(6, 'Password must be 6 character length')
@@ -51,8 +64,8 @@ const JwtLogin = () => {
   const theme = useTheme();
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
-
   const { login } = useAuth();
+  const isMobile = useMediaQuery(theme.breakpoints.down(700));
 
   const handleFormSubmit = async (values) => {
     setLoading(true);
@@ -65,102 +78,105 @@ const JwtLogin = () => {
   };
 
   return (
-    <JWTRoot>
-      <Card className="card">
-        <Grid container>
-          <Grid item sm={6} xs={12}>
-            <JustifyBox p={4} height="100%" sx={{ minWidth: 320 }}>
-              <img src="/assets/images/illustrations/dreamer.svg" width="100%" alt="" />
-            </JustifyBox>
-          </Grid>
+    <FullScreenWrapper>
+      {!isMobile && (
+        <HalfScreen style={{ background: 'lightgray' }}>
+          <HalfScreenContent>
+            <h1>Transforma tu manera de firmar fácil, rápido y digital</h1>
+            <Typography variant="body2" color="textSecondary">
+              En un mundo acelerado, firmar documentos al estilo tradicional puede retrasar tus
+              decisiones clave
+            </Typography>
+          </HalfScreenContent>
+        </HalfScreen>
+      )}
 
-          <Grid item sm={6} xs={12}>
-            <ContentBox>
-              <Formik
-                onSubmit={handleFormSubmit}
-                initialValues={initialValues}
-                validationSchema={validationSchema}
+      <FormScreen>
+        <Typography variant="h4" gutterBottom align="center">
+          Inicia sesión
+        </Typography>
+        <Typography
+          variant="body2"
+          color="textSecondary"
+          align="center"
+          style={{ marginBottom: '40px' }}
+        >
+          Lo utilizaremos para iniciar sesión o para crear una cuenta si aún no tienes una
+        </Typography>
+
+        <Formik
+          onSubmit={handleFormSubmit}
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+        >
+          {({ values, errors, touched, handleChange, handleBlur, handleSubmit }) => (
+            <form onSubmit={handleSubmit} style={{ width: '60%', maxWidth: '400px' }}>
+              <TextField
+                fullWidth
+                size="small"
+                type="email"
+                name="email"
+                label="Email"
+                variant="outlined"
+                onBlur={handleBlur}
+                value={values.email}
+                onChange={handleChange}
+                helperText={touched.email && errors.email}
+                error={Boolean(errors.email && touched.email)}
+                sx={{ mb: 3 }}
+              />
+              <TextField
+                fullWidth
+                size="small"
+                name="password"
+                type="password"
+                label="Password"
+                variant="outlined"
+                onBlur={handleBlur}
+                value={values.password}
+                onChange={handleChange}
+                helperText={touched.password && errors.password}
+                error={Boolean(errors.password && touched.password)}
+                sx={{ mb: 1.5 }}
+              />
+              <FlexBox justifyContent="space-between" sx={{ width: '100%', mb: 2 }}>
+                <FlexBox gap={1}>
+                  <Checkbox
+                    size="small"
+                    name="remember"
+                    onChange={handleChange}
+                    checked={values.remember}
+                    sx={{ padding: 0 }}
+                    style={{ color: '#7e7e7e' }}
+                  />
+                  <Typography variant="body2" style={{ color: '#7e7e7e' }}>
+                    Remember Me
+                  </Typography>
+                </FlexBox>
+                <NavLink to="/session/forgot-password" style={{ color: '#7e7e7e' }}>
+                  Forgot password?
+                </NavLink>
+              </FlexBox>
+              <LoadingButton
+                type="submit"
+                loading={loading}
+                variant="contained"
+                sx={{ my: 2, width: '100%' }}
+                style={{ backgroundColor: '#FF8548' }}
               >
-                {({ values, errors, touched, handleChange, handleBlur, handleSubmit }) => (
-                  <form onSubmit={handleSubmit}>
-                    <TextField
-                      fullWidth
-                      size="small"
-                      type="email"
-                      name="email"
-                      label="Email"
-                      variant="outlined"
-                      onBlur={handleBlur}
-                      value={values.email}
-                      onChange={handleChange}
-                      helperText={touched.email && errors.email}
-                      error={Boolean(errors.email && touched.email)}
-                      sx={{ mb: 3 }}
-                    />
-
-                    <TextField
-                      fullWidth
-                      size="small"
-                      name="password"
-                      type="password"
-                      label="Password"
-                      variant="outlined"
-                      onBlur={handleBlur}
-                      value={values.password}
-                      onChange={handleChange}
-                      helperText={touched.password && errors.password}
-                      error={Boolean(errors.password && touched.password)}
-                      sx={{ mb: 1.5 }}
-                    />
-
-                    <FlexBox justifyContent="space-between">
-                      <FlexBox gap={1}>
-                        <Checkbox
-                          size="small"
-                          name="remember"
-                          onChange={handleChange}
-                          checked={values.remember}
-                          sx={{ padding: 0 }}
-                        />
-
-                        <Paragraph>Remember Me</Paragraph>
-                      </FlexBox>
-
-                      <NavLink
-                        to="/session/forgot-password"
-                        style={{ color: theme.palette.primary.main }}
-                      >
-                        Forgot password?
-                      </NavLink>
-                    </FlexBox>
-
-                    <LoadingButton
-                      type="submit"
-                      color="primary"
-                      loading={loading}
-                      variant="contained"
-                      sx={{ my: 2 }}
-                    >
-                      Login
-                    </LoadingButton>
-
-                    <Paragraph>
-                      Don't have an account?
-                      <NavLink
-                        to="/session/signup"
-                        style={{ color: theme.palette.primary.main, marginLeft: 5 }}
-                      >
-                        Register
-                      </NavLink>
-                    </Paragraph>
-                  </form>
-                )}
-              </Formik>
-            </ContentBox>
-          </Grid>
-        </Grid>
-      </Card>
-    </JWTRoot>
+                Login
+              </LoadingButton>
+              <Typography variant="body2" align="center">
+                Don't have an account?
+                <NavLink to="/session/signup" style={{ color: '#fc7e3f' }}>
+                  Register
+                </NavLink>
+              </Typography>
+            </form>
+          )}
+        </Formik>
+      </FormScreen>
+    </FullScreenWrapper>
   );
 };
 
