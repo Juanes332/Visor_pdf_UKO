3a210aff5e207f2ebdc8b9164165daf31477b5f2
Juan Hernandez <juan.hernandez@gse.com.co>
2023-11-03 16:49:09 -0500

    Se agregó la ayuda para el usuario de hacer más grande y más pequeño

--- src/app/components/PDFViewer/SignatureField.jsx
+++ src/app/components/PDFViewer/SignatureField.jsx
@@ -21,10 +21,14 @@ function getRandomColor() {
 const SignatureField = ({ onRemove, initialPosition, onPositionChange, backgroundColor }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [signatureImage, setSignatureImage] = useState(null);
+  const [fieldBackground, setFieldBackground] = useState(getRandomColor());
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
-  const saveSignature = (signature) => setSignatureImage(signature);
+  const saveSignature = (signature) => {
+    setSignatureImage(signature);
+    setFieldBackground('transparent'); // Cambia el fondo a transparente al guardar la firma
+  };
 
   const fieldStyle = {
     border: '1px solid black',
@@ -34,9 +38,20 @@ const SignatureField = ({ onRemove, initialPosition, onPositionChange, backgroun
     justifyContent: 'center',
     alignItems: 'center',
     position: 'relative',
-    backgroundColor: backgroundColor,
-    backgroundImage: `url(${signatureImage})`, // Establecer la imagen de la firma como fondo
-    backgroundSize: 'cover',
+    backgroundColor: signatureImage ? 'transparent' : fieldBackground, // Usa el color de fondo solo si no hay imagen de firma
+    backgroundImage: `url(${signatureImage})`, // Establecer la imagen de la firma como fondo si está presente
+    backgroundSize: 'contain', // Asegúrate de que la imagen de la firma se ajuste al contenedor
+    backgroundRepeat: 'no-repeat', // No repetir la imagen de fondo
+    backgroundPosition: 'center', // Centrar la imagen de fondo
+  };
+
+  const resizeDotStyle = {
+    position: 'absolute',
+    width: '10px',
+    height: '10px',
+    background: '#fff',
+    border: '1px solid #007bff',
+    borderRadius: '50%', // Hace que tenga forma circular
   };
 
   return (
@@ -52,8 +67,15 @@ const SignatureField = ({ onRemove, initialPosition, onPositionChange, backgroun
           onPositionChange(d.x, d.y);
         }
       }}
+      enableResizing={{
+        topLeft: true, // Permitir cambiar tamaño desde la esquina superior izquierda
+        topRight: true, // Permitir cambiar tamaño desde la esquina superior derecha
+        bottomLeft: true, // Permitir cambiar tamaño desde la esquina inferior izquierda
+        bottomRight: true, // Permitir cambiar tamaño desde la esquina inferior derecha
+      }}
+      lockAspectRatio={true} // Mantener la relación de aspecto al cambiar el tamaño
     >
-      <div style={fieldStyle} onDoubleClick={openModal}>
+      <div style={{ ...fieldStyle, border: '1px solid #007bff' }} onDoubleClick={openModal}>
         {!signatureImage && <span>Signature</span>}
         <button
           style={{
@@ -69,6 +91,10 @@ const SignatureField = ({ onRemove, initialPosition, onPositionChange, backgroun
         >
           X
         </button>
+        <div style={{ ...resizeDotStyle, top: '-5px', left: '-5px' }} />
+        <div style={{ ...resizeDotStyle, top: '-5px', right: '-5px' }} />
+        <div style={{ ...resizeDotStyle, bottom: '-5px', left: '-5px' }} />
+        <div style={{ ...resizeDotStyle, bottom: '-5px', right: '-5px' }} />
       </div>
       <SignatureModal isOpen={isModalOpen} onClose={closeModal} onSave={saveSignature} />
     </Rnd>
